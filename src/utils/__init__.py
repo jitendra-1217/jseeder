74eec4268747fa273c75ccf9ad9e357133c3ee8c
jitendra-1217 <jitendraojha1217@gmail.com>
2016-10-13 23:38:30 +0530

    Cleaned master, will be rewriting all

--- src/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from src.utils.logger import logger
-from src.utils.cli import parseOptions, getCliHelpText
-from src.utils.yaml_parser import parseYamlFile
-from src.utils.try_catch_wrapper import tryCatchWrapper
--- src/utils/logger.py
+++
@@ -1,7 +0,0 @@
-# Logger util
-import logging
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="\n%(levelname)s - [%(asctime)s] %(filename)s:%(lineno)d - %(message)s"
-)
-logger = logging.getLogger(__name__)
